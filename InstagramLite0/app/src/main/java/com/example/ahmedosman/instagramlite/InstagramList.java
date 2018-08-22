45c7bef48c67ef81ba88b246093c63965a26940c
Ahmed Osman <ahmedhosman41@gmail.com>
2018-08-22 12:02:37 -0700

    finished

---
+++ InstagramLite0/app/src/main/java/com/example/ahmedosman/instagramlite/InstagramList.java
@@ -0,0 +1,16 @@
+package com.example.ahmedosman.instagramlite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InstagramList {
+    public static final List<InstagramObject> Koala = new ArrayList<>();
+    static {
+        Koala.add( new InstagramObject("Fluffy","http://i220.photobucket.com/albums/dd58/david_jaffe/Australia/MadKoala1.jpg", "The Destroyer of Worlds"));
+        Koala.add( new InstagramObject("Thanos","https://evil-koala.weebly.com/uploads/2/3/9/6/23967818/6738314.jpg", "I came, I saw, I conquered"));
+        Koala.add( new InstagramObject("Gnasher","https://scontent-sea1-1.cdninstagram.com/vp/2a5aae44c0c201d89be41c917d8187a4/5BFF97C2/t51.2885-15/sh0.08/e35/c1.0.1078.1078/s640x640/39752941_329688970932352_3639788507649540096_n.jpg", "The Great"));
+        Koala.add( new InstagramObject("Kuddles","https://scontent-sea1-1.cdninstagram.com/vp/c98546a3eea85ce4a244238e3313340f/5BFD673C/t51.2885-15/e35/39153625_303132066932689_8730819951784361984_n.jpg", "The Beast"));
+        Koala.add( new InstagramObject("Bob","https://scontent-sea1-1.cdninstagram.com/vp/a7cd5a8d8d954b9b44605a2881bb0624/5BFCD96F/t51.2885-15/e35/38941972_1934572156604979_2673396814232682496_n.jpg", "My Water"));
+        Koala.add( new InstagramObject("Spongebob","https://i.imgflip.com/1vqnnj.jpg","Deep contemplation"));
+    }
+}
---
+++ InstagramLite0/app/src/main/java/com/example/ahmedosman/instagramlite/InstagramObject.java
@@ -0,0 +1,13 @@
+package com.example.ahmedosman.instagramlite;
+
+public class InstagramObject {
+    public String name;
+    public String imageUrl;
+    public String motto;
+
+    public InstagramObject(String name, String url, String motto) {
+        this.name = name;
+        this.imageUrl = url;
+        this.motto = motto;
+    }
+}
